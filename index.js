cb11ae6efa4d7d28f5bbe1d95a328ee865afbbd2
Vladimir Radushevskiy <vrad-github@vrad.spb.ru>
2019-06-27 11:25:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ module.exports = {
   Body: Body,
   Vector2d: Vector2d,
   Body3d: Body3d,
-  Vector3d: Vector3d
+  Vector3d: Vector3d,
+  Body4d: Body4d,
+  Vector4d: Vector4d
 };
 
 function Body(x, y) {
@@ -62,4 +64,41 @@ function Vector3d(x, y, z) {
 
 Vector3d.prototype.reset = function () {
   this.x = this.y = this.z = 0;
+};
+
+
+// 4D fork
+
+function Body4d(x, y, z, t) {
+  this.pos = new Vector4d(x, y, z, t);
+  this.prevPos = new Vector4d(x, y, z, t);
+  this.force = new Vector4d();
+  this.velocity = new Vector4d();
+  this.mass = 1;
+}
+
+Body4d.prototype.setPosition = function (x, y, z, t) {
+  this.prevPos.x = this.pos.x = x;
+  this.prevPos.y = this.pos.y = y;
+  this.prevPos.z = this.pos.z = z;
+  this.prevPos.t = this.pos.t = t;
+};
+
+function Vector4d(x, y, z, t) {
+  if (x && typeof x !== 'number') {
+    // could be another vector
+    this.x = typeof x.x === 'number' ? x.x : 0;
+    this.y = typeof x.y === 'number' ? x.y : 0;
+    this.z = typeof x.z === 'number' ? x.z : 0;
+    this.t = typeof x.t === 'number' ? x.t : 0;
+  } else {
+    this.x = typeof x === 'number' ? x : 0;
+    this.y = typeof y === 'number' ? y : 0;
+    this.z = typeof z === 'number' ? z : 0;
+    this.t = typeof t === 'number' ? t : 0;
+  }
+};
+
+Vector4d.prototype.reset = function () {
+  this.x = this.y = this.z = this.t = 0;
 };
